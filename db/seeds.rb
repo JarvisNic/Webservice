39d01e027641dc132a2ff08a1e385041657750a8
JarvisNic <aaronmerk2loke9624@gmail.com>
2016-10-13 17:25:39 -0600

    seed for json

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Tablas.create(rango: 'A', sintoma: 'Dolor de Cabeza', valor: '2')
+Tablas.create(rango: 'B', sintoma: 'Dolor de Estomago', valor: '4')
+Tablas.create(rango: 'C', sintoma: 'Dolor de Vientre', valor: '4')
+Tablas.create(rango: 'D', sintoma: 'No toma cafe', valor: '5')
+Tablas.create(rango: 'E', sintoma: 'Estomago', valor: '56')
+Tablas.create(rango: 'F', sintoma: 'Darling', valor: '9')
+Tablas.create(rango: 'G', sintoma: 'Ganas de comer', valor: '7')
+Tablas.create(rango: 'H', sintoma: 'Ganas de Cagar', valor: '9')
+Tablas.create(rango: 'I', sintoma: 'Ganas de irme', valor: '1')
+Tablas.create(rango: 'J', sintoma: 'Soplete', valor: '22')
+Tablas.create(rango: 'K', sintoma: 'Damburgerg', valor: '0')
+Tablas.create(rango: 'L', sintoma: 'Azul', valor: '2')
+Tablas.create(rango: 'M', sintoma: 'BDPM', valor: '7')
+Tablas.create(rango: 'N', sintoma: 'AHMG', valor: '3')
+Tablas.create(rango: 'O', sintoma: 'Me dolio el golpe', valor: '10')
+Tablas.create(rango: 'P', sintoma: 'Yurizan Beltran', valor: '9')
+Tablas.create(rango: 'Q', sintoma: 'Viva el boer', valor: '25')
+Tablas.create(rango: 'R', sintoma: 'Mañana me cogen', valor: '7')
+Tablas.create(rango: 'S', sintoma: 'Copenhagen', valor: '2')
+
+
